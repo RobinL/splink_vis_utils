29587e84c650f40ef664e0031ed6124e18382607
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2021-12-15 08:38:58 +0000

    comparison viewer

--- update_pkg.sh
+++ update_pkg.sh
@@ -1,4 +1,3 @@
 npm install https://api.observablehq.com/d/5c0f40059131a129@290.tgz\?v\=3
-npm install https://api.observablehq.com/d/5c0f40059131a129.tgz\?v\=3
  ./node_modules/.bin/rollup -c
 ls node_modules/@robinl/to-embed-in-splink-outputs | grep @
